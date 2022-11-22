c5bfd7d473150f510ef0eafa65d3f501f9d9526a
MRRazvi <mrrazvi@icloud.com>
2022-11-22 19:37:45 +0500

    init

--- resources/views/pages/app/profile.blade.php
+++ resources/views/pages/app/profile.blade.php
@@ -49,23 +49,19 @@
                             <a class="nav-link" href="#password" data-toggle="tab">Password</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#2fa" data-toggle="tab">Two Factor</a>
+                            <a class="nav-link" href="#two-factor" data-toggle="tab">Two Factor</a>
                         </li>
                     </ul>
                 </div>
                 <div class="card-body">
                     <div class="tab-content">
                         <div class="tab-pane active" id="basic">
-                            @if ($errors->any())
+                            @foreach($errors->updateProfileInformation->all() as $error)
                                 <div class="alert alert-danger alert-dismissible">
                                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
+                                    {{ $error }}
                                 </div>
-                            @endif
+                            @endforeach
 
                             @if (session()->has('status') && session('status') == 'profile-information-updated')
                                 <div class="alert alert-success alert-dismissible">
@@ -124,7 +120,69 @@
                                 </div>
                             </form>
                         </div>
-                        <div class="tab-pane" id="security"></div>
+                        <div class="tab-pane" id="password">
+                            @foreach($errors->updatePassword->all() as $error)
+                                <div class="alert alert-danger alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                    {{ $error }}
+                                </div>
+                            @endforeach
+
+                            @if (session()->has('status') && session('status') == 'password-updated')
+                                <div class="alert alert-success alert-dismissible">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                    Your password has been successfully updated.
+                                </div>
+                            @endif
+
+                            <form action="{{ route('user-password.update') }}" method="POST" class="form-horizontal">
+                                @csrf
+                                @method('PUT')
+
+                                <div class="form-group row">
+                                    <label for="current_password" class="col-sm-2 col-form-label">Current Password</label>
+                                    <div class="col-sm-10">
+                                        <input
+                                            type="password"
+                                            class="form-control"
+                                            id="current_password"
+                                            placeholder="Current Password"
+                                            name="current_password"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="password" class="col-sm-2 col-form-label">New Password</label>
+                                    <div class="col-sm-10">
+                                        <input
+                                            type="password"
+                                            class="form-control"
+                                            id="password"
+                                            placeholder="New Password"
+                                            name="password"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="password_confirmation" class="col-sm-2 col-form-label">Confirm Password</label>
+                                    <div class="col-sm-10">
+                                        <input
+                                            type="password"
+                                            class="form-control"
+                                            id="password_confirmation"
+                                            placeholder="Confirm Password"
+                                            name="password_confirmation"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-sm-2 col-sm-10">
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="tab-pane" id="two-factor"></div>
                     </div>
                 </div>
             </div>
